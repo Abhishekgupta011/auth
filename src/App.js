18b2b7c85dc2c65f4a2707edc18f80e80b11f96e
Abhishekgupta011 <ag25061999@gmail.com>
2023-10-02 21:30:17 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Layout from './Components/Layout/Layout';
+import UserProfile from './Components/Profile/UserProfile';
+import HomePage from './Components/Pages/HomePage';
+import AuthPage from './Components/Pages/AuthPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path='/' exact element={<HomePage />}/>
+   
+        <Route path='/auth' element={<AuthPage />}/>
+  
+        <Route path='/profile' element={<UserProfile />}/>
+      </Routes>
+    </Layout>
   );
 }
 
